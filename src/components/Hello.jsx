8b50bc5c661ca2a95d58f69e3728a7f1942d7671
AldaAtitus <1137226@atitus.edu.br>
2026-05-15 20:16:53 -0300

    Atualização2

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,3 +1,9 @@
+function UserLogged() {
+    return ( <div>Usuário Logado</div> )
+}
+function DoLogin() {
+    return ( <div>Clique aqui e faça login</div> )
+}
 export function Hello({ isLogged = false, username = "" }) {
 
 }
