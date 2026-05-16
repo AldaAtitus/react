d31059556ea304f681469d061ba81a5bb750f0b8
AldaAtitus <1137226@atitus.edu.br>
2026-05-15 21:36:59 -0300

    Atualização7

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -2,7 +2,7 @@ function UserLogged({username}) {
     return ( <div>Seja bem-vindo, {username}!</div> )
 }
 function DoLogin({setUserState}) {
-    return ( <div onClick={() => setUserState(true)}>Clique aqui e faça login</div> )
+    return <button onClick={() => setUserState(true)}>Clique aqui e faça login</button>
 }
 export function Hello({ isLogged = false, username = "Aldacir", setUserState }) {
     return isLogged ? <UserLogged username={username} /> : <DoLogin setUserState={setUserState} />;
