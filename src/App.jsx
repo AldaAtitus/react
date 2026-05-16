92e6167ec5ba291cc97a83e6b643828030e80cc3
AldaAtitus <1137226@atitus.edu.br>
2026-05-15 21:21:44 -0300

    Atualização5

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import { Hello } from "./components/Hello";
 function App() {
-  return <Hello isLogged={true} username="Aldacir" />;
+  // inicializei um estado com o valor inicial false
+  // descontruição de array
+  const [userState, setUserState] = useState(false);
+  // Neste momento, usertState é false
+  setTimeout(function() {
+    setUserState(true);
+  }, 5000);
+  return <Hello isLogged={userState} username="Aldacir" />;
 }
-
 export default App
